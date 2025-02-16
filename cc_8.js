52b5b6b5ce26c35f3c1ecbb10695b58cc061f230
Julia <lorrayne@usf.edu>
2025-02-16 16:08:09 -0500

    Task 6 - Identifying Large Transactions

--- cc_8.js
+++ cc_8.js
@@ -94,3 +94,14 @@ function calculateLoanPayment(principal, rate, time) {
 console.log(calculateLoanPayment(1000, 0.05, 2)); 
 console.log(calculateLoanPayment(5000, 0.07, 3)); 
 // logged results to the console
+
+//Task 6: Higher-Order Functions
+let transactions = [200, 1500, 3200, 800, 2500]; // created array transactions with five values
+
+function filterLargeTransactions(transactions,filterFunction) {
+    let largeTransactions = transactions.filter(filterFunction);
+    return largeTransactions;
+};
+//created higher-order function filterLargeTransactions(transactions, filterFunction)to find transactions above $1000.
+
+console.log(filterLargeTransactions(transactions, amount => amount > 1000)); // logged results to the console
